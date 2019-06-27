e8b2a792c3844498c5595eead581b54970857602
John Smith <johnsmithcoder7@gmail.com>
2019-06-28 00:45:50 +0200

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,25 +1,71 @@
-const createRep = require('../utils').createVirtualRepresentation
-const magic = require('../handlers')
+const { createVirtualRepresentation: createRep } = require('../utils');
+const magic = require('../handlers');
 
-function rollback(bot, db, msg, commitID) {
-       let rep = await createRep(db, commitID);
-        
-       rep = rep.map(r => {
-           r.id = r._id
-           return r
-       });
-        
-       rep.forEach(async r => {
-          const opts = {
-              bot: bot,
-              guildID: msg.channel.guild.id,
-              type: r.type,
-              affectedID: r.id,
-              data: r.virtual
+async function rollback(bot, db, msg, commitID) {
+    let commit = await db.Log.findOne({ commitID });
+
+    if (commit.guildID != msg.channel.guild.id) return 'That commit does not belong to this guild!';
+
+    let reps = await createRep(db, commit._id);
+
+    reps = reps.map(r => {
+        r.id = r._id
+        return r
+    });
+
+    reps.forEach(async r => {
+        const opts = {
+            bot: bot,
+            guildID: msg.channel.guild.id,
+            type: r.type,
+            affectedID: r.id,
+            data: r.virtual
+        };
+
+        await magic(opts);
+    });
+}
+
+async function revert(bot, db, msg, commitID) {
+    let commit = await db.Log.findOne({ commitID });
+
+    if (commit.guildID != msg.channel.guild.id) return 'That commit does not belong to this guild!';
+
+    switch (commit.change) {
+        case 'create': {
+            if (commit.guildPart === 'channel') {
+                bot.deleteChannel(commit.partID, 'Revert Git action');
+            } else if (commit.guildPart === 'role') {
+                bot.deleteRole(commit.guildID, commit.partID, 'Revert git action');
+            }
+        }
+        case 'update': {
+            switch (commit.guildPart) {
+                case 'guild': {
+                    bot.editGuild(commit.guildID, commit.oldValue, 'Revert git action');
+                }
+                case 'channel': {
+                    bot.editChannel(commit.partID, commit.oldValue, 'Revert git action');
+                }
+                case 'role': {
+                    bot.editGuild(commit.guildID, commit.partID, commit.oldValue, 'Revert git action');
+                }
+            }
+        }
+        case 'delete': {
+            let reps = await createRep(db, commit._id);
+            let rep = reps.find(r => r._id === commit.partID);
+
+            const opts = {
+                bot: bot,
+                guildID: msg.channel.guild.id,
+                type: rep.type,
+                data: rep.virtual
             };
-            
-         await magic(opts);
-        });
+
+            await magic(opts);
+        }
+    }
 }
 
 module.exports = async (bot, db, msg) => {
@@ -31,11 +77,15 @@ module.exports = async (bot, db, msg) => {
         console.log(rep)
     } else if (msg.content.startsWith('!goto')) {
         const commitID = msg.content.replace('!goto ', '');
-     
+
         rollback(bot, db, msg, commitID);
     } else if (msg.content.startsWith('!rollback')) {
         const commitID = msg.content.replace('!rollback ', '');
-        
+
         rollback(bot, db, msg, commitID);
+    } else if (msg.content.startsWith('!revert')) {
+        const commitID = msg.content.replace('!revert ', '');
+
+
     }
 };
