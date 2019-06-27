cefc152ebefa2e73652b8ceed2d5b277ed974271
John Smith <johnsmithcoder7@gmail.com>
2019-06-27 17:03:35 -0400

    WIP rollback

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,6 +1,27 @@
 const createRep = require('../utils').createVirtualRepresentation
 const magic = require('../handlers')
 
+function rollback(bot, db, msg, commitID) {
+       let rep = await createRep(db, commitID);
+        
+       rep = rep.map(r => {
+           r.id = r._id
+           return r
+       });
+        
+       rep.forEach(async r => {
+          const opts = {
+              bot: bot,
+              guildID: msg.channel.guild.id,
+              type: r.type,
+              affectedID: r.id,
+              data: r.virtual
+            };
+            
+         await magic(opts);
+        });
+}
+
 module.exports = async (bot, db, msg) => {
     if (msg.content === '!ping') {
         bot.createMessage(msg.channel.id, 'Pong!');
@@ -9,21 +30,12 @@ module.exports = async (bot, db, msg) => {
         const rep = await createRep(db, suffix)
         console.log(rep)
     } else if (msg.content.startsWith('!goto')) {
-        const suffix = msg.content.replace('!goto ', '')
-        let rep = await createRep(db, suffix)
-        rep = rep.map(r => {
-            r.id = r._id
-            return r
-        })
-        rep.forEach(async r => {
-            const opts = {
-                bot: bot,
-                guildID: msg.channel.guild.id,
-                type: r.type,
-                affectedID: r.id,
-                data: r.virtual
-            }
-            await magic(opts)
-        })
+        const commitID = msg.content.replace('!goto ', '');
+     
+        rollback(bot, db, msg, commitID);
+    } else if (msg.content.startsWith('!rollback')) {
+        const commitID = msg.content.replace('!rollback ', '');
+        
+        rollback(bot, db, msg, commitID);
     }
 };
